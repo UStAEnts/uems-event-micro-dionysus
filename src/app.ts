cc3aca92aa2aa89ce04018a8d91f4983381b3ead
Ryan Delaney <vitineth@gmail.com>
2020-06-29 18:33:00 +0100

    Adjusting according to the new code style

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
+import * as fs from 'fs';
+import { RequestHandler } from 'express';
+import { Database } from './EventDetailsConnector';
+import { Messaging } from './messaging';
+import morgan from 'morgan';
 import express = require('express');
 import cookieParser = require('cookie-parser');
-import logger = require('morgan');
-
-import * as fs from 'fs';
-import { RequestHandler} from "express";
-
-import { Database } from "./EventDetailsConnector";
-import { Messaging } from "./messaging";
 
 // The topic used for messages destined to microservices of this type.
-const EVENT_DETAILS_SERVICE_TOPIC: string = "events.details.*";
+const EVENT_DETAILS_SERVICE_TOPIC: string = 'events.details.*';
 
 let eventsDb: Database.EventDetailsConnector | null = null;
 let messenger: Messaging.Messenger;
@@ -24,29 +22,55 @@ const asyncErrorCatcher = (fn: RequestHandler): RequestHandler => ((req, res, ne
     Promise.resolve(fn(req, res, next)).catch(next);
 });
 
-
 async function reqReceived(content: Buffer | null): Promise<string | null> {
     // TODO: checks for message integrity.
-    
-    console.log("Request received: " + content);
-    console.log("Handling request...");
+
+    console.log(`Request received: ${content}`);
+    console.log('Handling request...');
 
     if (content == null || eventsDb == null) {
         // Blank (null content) messages are ignored.
         // If the eventsDb connector is null then the microservice isn't ready and the message is dropped.
 
-        console.log("Message content null or DB not ready!, message dropped");
+        console.log('Message content null or DB not ready!, message dropped');
         return null;
     }
-    
+
     // Currently all events are returned, TODO query processing.
     const data = await eventsDb.retrieveAllEvents();
 
-    console.log("Response got");
+    console.log('Response got');
 
     return JSON.stringify(data);
 }
 
+console.log('Starting event micro dionysus...');
+
+export const app = express();
+
+app.set('port', process.env.PORT || 15550);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false,
+}));
+app.use(cookieParser());
+
+// Setup some listeners
+app.get('/test_get_events', asyncErrorCatcher(async (req, res) => {
+    if (eventsDb === null) {
+        console.error('failed to get events because database was null');
+        throw new Error('eventsDB was null');
+    }
+
+    eventsDb.retrieveAllEvents().then((data) => {
+        console.log(data);
+        res.json(data);
+    });
+}));
+
+app.post('/', (req, res) => res.send('Test Path, Post Req Received'));
 
 /**
  * Callback for the database being prepared. Will set events db instance and make the app listen on the port defined in
@@ -61,65 +85,44 @@ async function databaseConnectionReady(eventsConnection: Database.EventDetailsCo
     // Repeatedly attempt to connect to RabbitMQ messaging system.
     while (true) {
         try {
-            messenger = await Messaging.Messenger.setup("rabbit-mq-config.json", reqReceived, [EVENT_DETAILS_SERVICE_TOPIC]);
+            // no-await-in-loop: used to retry an action, ignored as per https://eslint.org/docs/rules/no-await-in-loop
+            // @typescript-eslint/no-unused-vars: kept to retain code, TODO: is usage required?
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-await-in-loop
+            messenger = await Messaging.Messenger.setup(
+                'rabbit-mq-config.json',
+                reqReceived,
+                [EVENT_DETAILS_SERVICE_TOPIC],
+            );
             break;
-        } catch(err) {
-            console.log("Attempting to reconnect to RabbitMQ....");
-            await new Promise(resolve => setTimeout(resolve, 2000));
+        } catch (err) {
+            console.log('Attempting to reconnect to RabbitMQ....');
+
+            // no-await-in-loop: used to retry an action, ignored as per https://eslint.org/docs/rules/no-await-in-loop
+            // eslint-disable-next-line no-await-in-loop
+            await new Promise((resolve) => setTimeout(resolve, 2000));
         }
     }
 
-    app.listen(process.env.PORT, function () {
-        console.log("Event micro dionysus started successfully");
+    app.listen(process.env.PORT, () => {
+        console.log('Event micro dionysus started successfully');
     });
 }
 
-console.log("Starting event micro dionysus...");
-
-export const app = express();
-
-app.set('port', process.env.PORT || 15550);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: false,
-}));
-app.use(cookieParser());
-
-// Setup some listeners
-app.get('/test_get_events', asyncErrorCatcher(async (req, res, next) => {
-    if (eventsDb === null) {
-        console.error('failed to get events because database was null');
-        throw new Error('eventsDB was null');
-    }
-
-    eventsDb.retrieveAllEvents().then(function (data) {
-        console.log(data);
-        res.json(data);
-    });
-}))
-
-
-app.post('/', (req, res) => {
-    return res.send("Test Path, Post Req Received")
-});
-
 // Start of the mongoDB connection.
 // The connection info (including username / password) is read from configuration file.
-fs.readFile("database.json", { encoding: "utf-8" }, function (err, data) {
+fs.readFile('database.json', { encoding: 'utf-8' }, (err, data) => {
     if (err) {
-        console.error("Failed to read database.json file... database connection failed...");
+        console.error('Failed to read database.json file... database connection failed...');
         return;
     }
 
-    console.log("Database.json file opened successfully...");
+    console.log('Database.json file opened successfully...');
 
-    const db_info = JSON.parse(data);
+    const dbInfo = JSON.parse(data);
 
-    Database.connect(db_info.uri).then(databaseConnectionReady).catch((err) => {
-        console.error("Event details microservice failed to connect to database... exiting");
-        console.error(err.message);
+    Database.connect(dbInfo.uri).then(databaseConnectionReady).catch((e) => {
+        console.error('Event details microservice failed to connect to database... exiting');
+        console.error(e.message);
         process.exit(1);
     });
 });
