bdfb7037e01b38844cf6e9f15e769212fba396f0
Paul Lancaster <paul@lancasterzone.com>
2020-07-09 16:02:03 +0100

    Added mocha testing

--- src/messaging.ts
+++ src/messaging.ts
@@ -1,6 +1,85 @@
 import { Channel, connect as amqpConnect, Connection } from 'amqplib/callback_api';
+import Ajv from 'ajv';
 
 const fs = require('fs').promises;
+
+let schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "$id": "https://entscrew.net/schemas/msg_schema.json",
+    "type": "object",
+    "required": ["msg_id", "status", "msg_intention"],
+    "anyOf": [
+        {"required": ["event_name", "event_start_date", "event_end_date", "venue_ids", "attendance"]},
+    ],
+    "properties": {
+        "items": {
+            "type": "object",
+            "additionalProperties": false,
+            "items": {
+                "msg_id": {
+                    "type": "string",
+                    "description": "An ID for this message which is unique within the system"
+                },
+                "status": {
+                    "type": "number",
+                    "description": "The status of the message, uses HTTP status codes, 0 value if unset"
+                },
+                "anyOf": [
+                    {
+                        "type": "object",
+                        "required": ["event_name", "event_start_date", "event_end_date", "venue_ids", "attendance"],
+                        "properties": {
+                            "msg_intention": {
+                                "type": "string",
+                                "enum": [ "CREATE" ],
+                                "description": "The purpose / intention of the message"
+                            },
+                            "event_id": {
+                                "type": "string",
+                                "description": "The unique ID of the event to modify"
+                            },
+                            "event_name": {
+                                "type": "string",
+                                "description": "The new human-readable non-unique name of the event"
+                            },
+                            "event_start_date": {
+                                "type": "number",
+                                "description": "The new event start_date as a UTC timestamp in seconds since epoch"
+                            },
+                            "event_end_date": {
+                                "type": "number",
+                                "description": "The new event end_date as a UTC timestamp in seconds since epoch"
+                            },
+                            "venue_ids": {
+                                "type": "array",
+                                "items": {
+                                    "type": "string",
+                                    "description": "The new unique venue IDs for all venues this event is occuring in"
+                                }
+                            },
+                            "attendance": {
+                                "type": "number",
+                                "description": "The new attendance value for the event"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    }
+};
+
+let data = {
+	"msg_id": "1",
+    "status": 200,
+    "msg_intention": "CREATE",
+    "event_id": "evId",
+    "event_name": "evName",
+    "event_start_date": 100000,
+    "event_end_date": 100002,
+    "venue_ids": ["1", "2"],
+    "attendance": 140
+};
 
 // Represents the RabbitMQ config file in memory.
 type MqConfig = {
@@ -16,6 +95,11 @@ const GATEWAY_EXCHANGE: string = 'gateway';
 // The exchange used for receiving requests to microservices.
 const REQUEST_EXCHANGE: string = 'request';
 
+// A path to the .json file which describes valid message schema.
+const MESSAGE_SCHEMA_PATH: string = '../schema/event_schema.json';
+
+let compiled_message_schema = null;
+
 export namespace Messaging {
     export class Messenger {
         // Connection to the RabbitMQ messaging system.
@@ -26,10 +110,13 @@ export namespace Messaging {
 
         rcv_ch: Channel;
 
-        private constructor(conn: Connection, sendCh: Channel, rcvCh: Channel) {
+        schema_validator: Ajv.ValidateFunction;
+
+        private constructor(conn: Connection, sendCh: Channel, rcvCh: Channel, schema_validator: Ajv.ValidateFunction) {
             this.conn = conn;
             this.send_ch = sendCh;
             this.rcv_ch = rcvCh;
+            this.schema_validator = schema_validator;
         }
 
         // Once a request has been handled and the response returned this method takes that request message and the
@@ -59,6 +146,7 @@ export namespace Messaging {
             conn: Connection,
             reqRecvCallback: (content: any) => Promise<string | null>,
             topics: [string],
+            schema_validator: Ajv.ValidateFunction
         ) {
             conn.on('error', (err: Error) => {
                 if (err.message !== 'Connection closing') {
@@ -116,7 +204,7 @@ export namespace Messaging {
                                     Messaging.Messenger.sendResponse(contentJson, res, sendCh);
                                 }, { noAck: true });
 
-                            resolve(new Messaging.Messenger(conn, sendCh, rcvCh));
+                            resolve(new Messaging.Messenger(conn, sendCh, rcvCh, schema_validator));
                         });
                     });
                 });
@@ -130,19 +218,23 @@ export namespace Messaging {
         static setup(
             configPath: string,
             reqRecvCallback: (content: any) => Promise<string | null>,
-            topics: [string],
+            topics: [string]
         ) {
             return new Promise<Messenger>((resolve, reject) => {
-                console.log('Connecting to rabbitmq...');
-                fs.readFile(configPath).then((data: Buffer) => {
-                    const configJson: MqConfig = JSON.parse(data.toString());
-                    amqpConnect(`${configJson.uri}?heartbeat=60`, async (err: Error, conn: Connection) => {
-                        if (err) {
-                            console.error('[AMQP]', err.message);
-                            reject(err);
-                            return;
-                        }
-                        resolve(await Messenger.configureConnection(conn, reqRecvCallback, topics));
+                fs.readFile(MESSAGE_SCHEMA_PATH).then((schemaRaw: Buffer) => {
+                    let ajv = new Ajv();
+                    let schema = ajv.compile(schemaRaw.toJSON());
+                    console.log('Connecting to rabbitmq...');
+                    fs.readFile(configPath).then((data: Buffer) => {
+                        const configJson: MqConfig = JSON.parse(data.toString());
+                        amqpConnect(`${configJson.uri}?heartbeat=60`, async (err: Error, conn: Connection) => {
+                            if (err) {
+                                console.error('[AMQP]', err.message);
+                                reject(err);
+                                return;
+                            }
+                            resolve(await Messenger.configureConnection(conn, reqRecvCallback, topics, schema));
+                        });
                     });
                 });
             });
