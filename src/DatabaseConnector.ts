105c298889c4379060c8663784014e507735ef5d
Ryan Delaney <vitineth@gmail.com>
2020-12-28 01:14:48 +0000

    Removing unused blocks and adding new configuration validation system
    This should make it a bit more flexible

--- src/DatabaseConnector.ts
+++ src/DatabaseConnector.ts
@@ -1,112 +1,49 @@
+import * as zod from 'zod';
 import * as MongoClient from 'mongodb';
-
-// The collection within the event database which contains the event details.
-const EVENT_DETAILS_COLLECTION = 'details';
-
-// The database used for storing events.
-const EVENT_DB = 'events';
+import { EventDatabaseInterface } from './database/type/impl/EventDatabaseInterface';
+import { EntStateDatabaseInterface } from './database/type/impl/EntStateDatabaseInterface';
 
 export namespace Database {
 
-    // Represents a UemsEvent that has been retrieved from the database.
-    export type UemsEvent = {
-        id: string,
-        name: string,
-        start_date: number,
-        end_date: number,
-        venue: string[]
+    const CONFIG_VALIDATOR = zod.object({
+        uri: zod.string(),
+        auth: zod.object({
+            user: zod.string(),
+            password: zod.string(),
+        }),
+        mapping: zod.object({
+            event: zod.string(),
+            ent: zod.string(),
+        }),
+        options: zod.any().optional(),
+    })
+        .nonstrict();
+
+    export type DatabaseConnections = {
+        event: EventDatabaseInterface,
+        ent: EntStateDatabaseInterface,
     };
-
-    // Represents a query to the database for information.
-    // If all fields are undefined it indicates that all data should be returned.
-    export type UemsQuery = {
-        id?: string,
-        name?: string,
-        start_date?: number,
-        end_date?: number,
-        venue?: string[]
-    };
-
-    // The result of an insert event request.
-    export type InsertEventResult = {
-        event_id?: string, // Event ID of the inserted event, only populated if ok.
-        err_msg?: string // Error message, only populated if err.
-    };
-
-    // Represents the database connection to mongoDB and handles requests to the database.
-    export class DatabaseConnector {
-        constructor(private db: MongoClient.Db) {
-            this.db = db;
-        }
-
-        // Retrieves data from the database matching the given query.
-        async retrieveQuery(query: UemsQuery): Promise<Database.UemsEvent[]> {
-            const collection = this.db.collection(EVENT_DETAILS_COLLECTION);
-            const res: UemsEvent[] = await collection.find(query).toArray();
-            return res;
-        }
-
-        // Inserts the given event into the database.
-        // Note that the ID of the event will be ignored as a new ID is generated on insertion.
-        async insertEvent(content: UemsEvent): Promise<InsertEventResult> {
-            const collection = this.db.collection(EVENT_DETAILS_COLLECTION);
-            const res = await collection.insertOne(content);
-
-            let ret: InsertEventResult;
-
-            if (res.result !== undefined) {
-                ret = {
-                    event_id: res.insertedId,
-                };
-            } else {
-                ret = {
-                    err_msg: 'Database failed to insert event',
-                };
-            }
-
-            return ret;
-        }
-
-        // Modifies the event with the given eventId to match the new values in newEvent.
-        // Returns true if successful, false if not.
-        async findAndModifyEvent(eventId: string, newEvent: any): Promise<boolean> {
-            // TODO, setup the database so changes are timestamped in a reversable way.
-            const collection = this.db.collection(EVENT_DETAILS_COLLECTION);
-
-            const res = await collection.replaceOne(
-                {
-                    _id: new MongoClient.ObjectID(eventId),
-                },
-                newEvent,
-            );
-
-            return (res.result.ok !== undefined);
-        }
-
-        // Removes an event from the database.
-        // Returns true if successful, false if not.
-        async removeEvent(eventId: string): Promise<boolean> {
-            // TODO, setup the database so changes are timestamped in a reversable way.
-            const collection = this.db.collection(EVENT_DETAILS_COLLECTION);
-            const res: MongoClient.DeleteWriteOpResultObject = await collection.deleteOne(
-                {
-                    _id: new MongoClient.ObjectID(eventId),
-                },
-            );
-
-            return (res.result.ok !== undefined);
-        }
-    }
 
     // Connects the datebase connect to the mongoDB database at the given uri.
-    export async function connect(uri: string): Promise<DatabaseConnector> {
+    export async function connect(config: any): Promise<DatabaseConnections> {
+        const validate = CONFIG_VALIDATOR.safeParse(config);
+        if (!validate.success) {
+            throw new Error(`Failed to validate config: ${JSON.stringify(validate.error.flatten())}`);
+        }
+        const configuration = validate.data;
+
         try {
-            const client = await MongoClient.connect(uri, {
+            const client = await MongoClient.connect(configuration.uri, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
+                auth: configuration.auth,
+                ...configuration.options,
             });
 
-            return new DatabaseConnector(client.db(EVENT_DB));
+            return {
+                event: new EventDatabaseInterface(client.db(configuration.mapping.event)),
+                ent: new EntStateDatabaseInterface(client.db(configuration.mapping.ent)),
+            };
         } catch (e) {
             console.log('failed to connect to the database', e);
             throw e;
