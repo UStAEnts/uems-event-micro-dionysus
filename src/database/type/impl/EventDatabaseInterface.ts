1b38d86b54c6b33d8600bdfe95e24766e935a341
Ryan Delaney <vitineth@gmail.com>
2021-01-03 02:34:34 +0000

    Adding new modify function. This is a bit of a shim so will need redoing
    This also includes the changelog pulling but has not been implemented yet

--- src/database/type/impl/EventDatabaseInterface.ts
+++ src/database/type/impl/EventDatabaseInterface.ts
@@ -1,5 +1,7 @@
-import { Db } from 'mongodb';
+import { Db, ObjectID } from 'mongodb';
 import { DefaultInterface } from './DefaultInterface';
+import { EventResponse } from "@uems/uemscommlib";
+import InternalEvent = EventResponse.InternalEvent;
 
 const EVENT_DETAILS_COLLECTION = 'details';
 const EVENT_CHANGELOG_COLLECTION = 'changelog';
@@ -23,9 +25,43 @@ export type Event = {
     state?: string,
 };
 
-export class EventDatabaseInterface extends DefaultInterface<EventQuery, Event> {
+// TODO: move to uems comms library
+export type Changelog = {
+    id: string,
+    occurred: number,
+    change: string,
+    user?: string,
+};
+
+export class EventDatabaseInterface extends DefaultInterface<any, InternalEvent> {
 
     constructor(db: Db) {
         super(db, EVENT_DETAILS_COLLECTION, EVENT_CHANGELOG_COLLECTION);
+    }
+
+    // TODO: provide a better way to do this
+    modify = async (id: string, updated: any): Promise<boolean> => {
+        const result = await this._objects.updateOne({
+            _id: new ObjectID(id),
+        }, updated);
+
+        if (result && result.matchedCount === 1) {
+            await super.log(id, 'updated', { changes: updated.$set });
+
+            return true;
+        }
+
+        return false;
+    }
+
+    async retrieveChangelog(eventID: string): Promise<Changelog[]> {
+        return (await super._changelog.find({
+            id: eventID,
+        })
+            .toArray()).map((value) => ({
+            change: value.action,
+            id: value._id,
+            occurred: value.timestamp,
+        }));
     }
 }
